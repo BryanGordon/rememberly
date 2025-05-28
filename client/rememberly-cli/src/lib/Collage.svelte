ad9570cfec55a157e9a5571d63b9d5336ad625ef
Bryan Gordon <bryanbge92@gmail.com>
2025-05-27 20:15:28 -0500

    Create new component

---
+++ client/rememberly-cli/src/lib/Collage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+</script>
+
+<section>
+  <article>
+    <div>
+      <img src="" alt="imagen">
+    </div>
+  </article>
+</section>
+
+<style>
+  section {
+    width: 80%;
+    padding: 10px;
+  }
+
+  article {
+    display: grid;
+  }
+</style>
