82aa3c6a0065a30a7b4d197e6a4e073ef67f35b9
Bryan Gordon <bryanbge92@gmail.com>
2025-06-02 20:12:28 -0500

    Add api connection

--- client/rememberly-cli/src/lib/Collage.svelte
+++ client/rememberly-cli/src/lib/Collage.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Image from '../assets/pepe.jpg'
   import Image1 from '../assets/profile.jpg'
+
+  const data = fetch('http://localhost:3000/images')
+    .then(res => res.json())
 </script>
 
 <section>
@@ -20,6 +23,20 @@
     <div>
       <img src={Image1} alt="imagen">
     </div>
+  </article>
+
+  <h1 class="middle-api">Datos desde la api</h1>
+
+  <article>
+    {#await data}
+      <h4>Cargando...</h4>
+    {:then images}
+      {#each images as image}
+        <div>
+          <img src={image.image} alt={image.alt}>
+        </div>
+      {/each}
+    {/await}
   </article>
 </section>
 
@@ -48,6 +65,18 @@
     border-radius: 15px;
     box-shadow: 2px 2px 10px #181818;
     transition: transform 0.2s ease-in;
+  }
+
+  .middle-api {
+    text-align: center;
+    color: yellowgreen;
+    margin-block: 30px;
+  }
+
+  h4 {
+    color: skyblue;
+    text-align: center;
+    font-weight: bold;
   }
   
 </style>
