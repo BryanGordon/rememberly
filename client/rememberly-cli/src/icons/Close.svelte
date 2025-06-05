4c45fea10b93c66ffe6bcb11dd1ac5ba992f237c
Bryan Gordon <bryanbge92@gmail.com>
2025-06-04 19:43:39 -0500

    Add Icon

---
+++ client/rememberly-cli/src/icons/Close.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<svg 
+  fill="none"
+  width="30"
+  height="30"
+  viewBox="0 0 24 24" 
+  stroke-width="1.5" 
+  stroke="currentColor" 
+  >
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
