8d719378cb38e17f73b5aa4c02fb592bf1cec363
Bryan Gordon <bryanbge92@gmail.com>
2025-06-04 19:43:12 -0500

    Update main file
    - Update structure

--- client/rememberly-cli/src/App.svelte
+++ client/rememberly-cli/src/App.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
   import Collage from './lib/Collage.svelte';
+
+  import Image1 from './assets/profile.jpg'
+    import Close from './icons/Close.svelte';
+
 </script>
 
 <main>
+  <section>
+
   <div>
     <h1>Rememberly</h1>
     <h3>Vuelve a recordar todos tus buenos recuerdos</h3>
   </div>
   <Collage />
+
+</section>
 </main>
 
 <footer>Derechos reservados</footer>
 
 <style>
-  main {
+  section {
     margin: 30px;
   }
 
