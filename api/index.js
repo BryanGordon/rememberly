a104398ef84766063e8c89ee10143dd6b31c3d63
Bryan Gordon <bryanbge92@gmail.com>
2025-06-11 14:09:19 -0500

    Add db connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import Images from './images.json' with {type: 'json'}
 import { corsMiddleware } from './middleware/cors.js'
+import postgres from 'postgres'
+import 'dotenv/config'
 
-const app = express()
 const allImages = Images
+const connection = process.env.DATABASE_URL
+const sql = postgres(connection)
+const app = express()
 
 app.use(corsMiddleware())
 app.use(express.json())
@@ -11,9 +15,18 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({message: 'Inicio del server'})
 })
-
+/*
 app.get('/images', (req, res) => {
   res.json(allImages)
+})
+*/
+
+// Conexion con la bdd
+app.get('/images', (req, res) => {
+  sql`SELECT * FROM Images`
+    .then(images => {
+      res.json(images)
+    })
 })
 
 const PORT = process.env.PORT ?? 3000
