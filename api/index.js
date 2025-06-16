da706d4a5b588e8669a0a72c20420bf3851c09e8
Bryan Gordon <bryanbge92@gmail.com>
2025-06-16 18:26:05 -0500

    Delete local connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
-import Images from './images.json' with {type: 'json'}
 import { corsMiddleware } from './middleware/cors.js'
 import postgres from 'postgres'
 import 'dotenv/config'
 
-const allImages = Images
 const connection = process.env.DATABASE_URL
 const sql = postgres(connection, {
   ssl: 'require'
@@ -17,13 +15,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({message: 'Inicio del server'})
 })
-/*
-app.get('/images', (req, res) => {
-  res.json(allImages)
-})
-*/
 
-// Conexion con la bdd
 app.get('/images', async (req, res) => {
   const images = await sql`SELECT * FROM images`
   res.json(images)
