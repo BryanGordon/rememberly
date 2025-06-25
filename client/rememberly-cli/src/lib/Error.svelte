717d0031969e9eb8b6f98c1d3c32d089e8cc28f6
Bryan Gordon <bryanbge92@gmail.com>
2025-06-24 20:29:08 -0500

    Add styles for error component

--- client/rememberly-cli/src/lib/Error.svelte
+++ client/rememberly-cli/src/lib/Error.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-
+  import Warning from '../icons/Warning.svelte'
 </script>
 
-<h1>
-  Error
-</h1>
+<div>
+  <h2>Error <Warning /></h2>
+  <p>No se pudo mostrar las imagenes, intentelo mas tarde.</p>
+</div>
+
+<style>
+  h2 {
+    color: rgba(220, 20, 60, 0.766);
+    font-size: 40px;
+    text-align: center;
+  }
+  
+  p {
+    color: rgb(220, 20, 60);
+    font-size: 20px;
+    text-align: center;
+  }
+</style>
