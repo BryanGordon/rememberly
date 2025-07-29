ab7cab1dd29ff805be0b84d7ab67de3ee2fcd6d8
Bryan Gordon <bryanbge92@gmail.com>
2025-07-28 21:47:50 -0500

    Add default port

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/images', routerImages)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
