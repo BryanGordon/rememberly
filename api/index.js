9cdef4d8e820db96e446000ca6e7c2b1e7baec51
Bryan Gordon <bryanbge92@gmail.com>
2025-07-28 21:48:49 -0500

    Serve local assets via /assets endpoint
    Added static middleware to serve files from the 'assets' directory under the /assets route. This enables access to local assets such as images through the API.

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express, { json } from 'express'
 import { corsMiddleware } from './middleware/cors.js'
 import { routerImages } from './routes/routesImages.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 
 app.use(corsMiddleware())
 app.use(json())
+/** Code for use local assets (e.g imgs) on api */
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 app.get('/', (req, res) => {
   res.json({message: 'Inicio del server'})
